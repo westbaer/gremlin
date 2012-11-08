29fd22d8e73c825bbcab6afe640a0ba5c2afaf8a
Nicolas Haunold <westbaer@gmail.com>
2012-11-08 23:43:21 +0100

    initial preferences commit

--- framework/prefs/prefs.mm
+++ framework/prefs/prefs.mm
@@ -1,13 +1,13 @@
 #import <Preferences/PSListController.h>
 
-@interface prefsListController: PSListController {
+@interface GRPreferencesListController: PSListController {
 }
 @end
 
-@implementation prefsListController
+@implementation GRPreferencesListController
 - (id)specifiers {
 	if(_specifiers == nil) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"prefs" target:self] retain];
+		_specifiers = [[self loadSpecifiersFromPlistName:@"GRPreferences" target:self] retain];
 	}
 	return _specifiers;
 }
